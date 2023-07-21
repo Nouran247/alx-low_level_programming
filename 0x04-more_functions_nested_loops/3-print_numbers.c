362096db019ce62665e4d9bce285ffcd7e10b1b0
Nouran247 <noura2004chan@gmail.com>
2023-07-20 17:58:14 -0700

    MORE

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include"main.h"
+
+/**
+ * print_numbers - prints the numbers, from 0 to 9, followed by a new line
+ *
+ * Return: Always 0 (Success)
+*/
+
+void print_numbers(void)
+{
+	int n = 0;
+
+	do {
+		_putchar(n + 48);
+		n++;
+	} while (n >= 0 && n <= 9);
+	_putchar('\n');
+}
