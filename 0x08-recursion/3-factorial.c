9552fae85e2895ed72ba1e40fe975826193874ab
Nouran247 <noura2004chan@gmail.com>
2023-08-02 15:02:03 -0700

    Recursion

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * factorial - findes factorial
+ * @n: int
+ * Return: int
+*/
+
+int factorial(int n)
+{
+	if (n <= 1)
+	{
+	return (1);
+	}
+
+return (n * factorial(n - 1));
+}
